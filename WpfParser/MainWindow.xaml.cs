2b6666924929a28d97b37fefa4ef8560397a4432
SystemX <SystemX@DESKTOP-7JLQH1M>
2024-03-12 21:32:47 +0300

    Добавьте файлы проекта.

---
+++ WpfParser/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfParser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Provider.Get_ProductPath(text_box.Text);
+            Provider.price += Provider.Get_Price(path);
+            label_box.Content = Provider.price + " BYN";
+        }
+    }
+}
---
+++ WpfParser/Provider.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfParser
+{
+    public class Provider
+    {
+        public static double price = 0;
+        public static string Get_Name(string path)
+        {
+            var request = new GetRequest(path);
+            request.Run();
+            string response = request.Response;
+
+            string meta_description = "<meta property=\"og:title\" content=\"";
+            int meta_start = response.IndexOf(meta_description) + meta_description.Length;
+            int meta_end = response.IndexOf("\"", meta_start);
+            string product_name = response.Substring(meta_start, meta_end - meta_start);
+
+            return product_name;
+        }
+
+        public static double Get_Price(string path)
+        {
+            var request = new GetRequest(path);
+            request.Run();
+            string response = request.Response;
+
+            var json = JObject.Parse(response);
+            double price = Convert.ToDouble(json["prices"]["current"]["amount"]);
+
+            return price;
+        }
+
+        public static string Get_ProductPath(string path)
+        {
+            string main_path = "https://catalog.api.onliner.by/products/";
+            int index = path.LastIndexOf('/');
+            index++;
+            for (; index < path.Length; index++)
+            {
+                main_path += path[index];
+            }
+            main_path += "/prices-history?period=2m";
+            return main_path;
+        }
+    }
+}
